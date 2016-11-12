0e3bcb3442ffe7637528bbf738a1ff40dc4f8f5a
j201 <j201.alex@gmail.com>
2016-11-12 15:52:34 -0500

    Use nalgebra for geometry and add random movement

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,42 @@
 extern crate piston_window;
+extern crate nalgebra as na;
+extern crate rand;
 
 use piston_window::*;
 use std::ops::*;
+use rand::distributions::IndependentSample;
+use rand::distributions as rdist;
 
-const ball_speed: f64 = 1.0;
+const MAX_V: f64 = 2.0;
+
+type Vect = na::Vector2<f64>;
+type Point = na::Point2<f64>;
 
 struct Ball {
-	x: f64,
-	y: f64,
-	vx: f64,
-	vy: f64,
-	r: f64
+	p: Point,
+	v: Vect,
+	r: f64,
+	rot: f64,
+	a_trans: Vect,
+	a_rot: f64,
+}
+
+impl Ball {
+	fn init_at_origin(r: f64) -> Ball {
+		Ball {
+			p: Point::new(0.0, 0.0),
+			v: Vect::new(0.0, 0.0),
+			r: r,
+			rot: 0.0,
+			a_trans: Vect::new(0.0, 0.0),
+			a_rot: 0.0
+		}
+	}
 }
 
 // Assumes one 'wrap' will always be enough (the result is always within the bounds)
 // Apparently generic numerical functions are pretty painful
-fn bound_wrap<T: Add<Output=T> + Sub<Output=T> + PartialOrd + Copy>(x: T, min: T, max: T) -> T {
+fn bound_reflect<T: Add<Output=T> + Sub<Output=T> + PartialOrd + Copy>(x: T, min: T, max: T) -> T {
 	if x > max {
 		max - (x - max)
 	} else if x < min {
@@ -25,20 +46,39 @@ fn bound_wrap<T: Add<Output=T> + Sub<Output=T> + PartialOrd + Copy>(x: T, min: T
 	}
 }
 
-impl Ball {
-	fn bounces(&self, dx: f64, dy: f64) -> bool {
-		self.x + dx > 1.0 || self.x + dx < -1.0 || self.y + dy > 1.0 || self.y + dy < -1.0
+fn wrap<T: Add<Output=T> + Sub<Output=T> + PartialOrd + Copy>(x: T, min: T, max: T) -> T {
+	let mut y = x;
+	while y > max {
+		y = y - (max - min);
 	}
+	while y < min {
+		y = y + max - min;
+	}
+	y
+}
+
+fn bound<T: PartialOrd + Copy>(x: T, min: T, max: T) -> T {
+	if x < min {
+		min
+	} else if x > max {
+		max
+	} else {
+		x
+	}
+}
+
+impl Ball {
 	fn update(&self, dt: f64) -> Ball {
-		let x2 = self.x + self.vx*dt;
-		let y2 = self.y + self.vy*dt;
-		let bounce = self.bounces(self.vx*dt, self.vy*dt);
+		// TODO: global seeding
+		let mut rng = rand::thread_rng();
+		let range = rdist::Range::new(-0.01, 0.01);
 		Ball {
-			x: bound_wrap(self.x + self.vx*dt, -1.0, 1.0),
-			y: bound_wrap(self.y + self.vy*dt, -1.0, 1.0),
-			vx: if bounce { -self.vx } else { self.vx },
-			vy: if bounce { -self.vy } else { self.vy },
-			.. *self
+			p: self.p + self.v*dt,
+			v: self.v + self.a_trans*dt,
+			r: self.r,
+			rot: wrap(self.rot + self.a_rot*dt, -1.0, 1.0),
+			a_trans: self.a_trans + Vect::new(range.ind_sample(&mut rng), range.ind_sample(&mut rng)),
+			a_rot: self.a_rot + range.ind_sample(&mut rng)
 		}
 	}
 }
@@ -50,21 +90,7 @@ struct App {
 impl App {
 	fn init() -> App {
 		App {
-			balls: [
-				Ball {
-					x: -1.0,
-					y: 0.5,
-					vx: ball_speed,
-					vy: 0.0,
-					r: 0.05
-				}, Ball {
-					x: 0.5,
-					y: -0.5,
-					vx: 0.0,
-					vy: ball_speed,
-					r: 0.05
-				}
-			]
+			balls: [Ball::init_at_origin(0.03), Ball::init_at_origin(0.03)]
 		}
 	}
 }
@@ -86,9 +112,8 @@ fn main() {
 		window.draw_2d(&e, |c, g| {
 			clear(color::WHITE, g);
 			for b in app.balls.into_iter() {
-				let x = (b.x/2.0+0.5)*400.0;
-				let y = (b.y/2.0+0.5)*400.0;
-				// println!("x: {}, y: {}", x, y);
+				let x = (b.p.x/2.0+0.5)*400.0;
+				let y = (b.p.y/2.0+0.5)*400.0;
 				Ellipse::new([1.0, 0.0, 0.0, 0.5])
 					.draw(ellipse::circle(x, y, b.r*400.0), &c.draw_state, c.transform, g);
 			}
